33a702c7e3ed1a7221b0e6e48f758bc8b6de5e1f
Paweł Nadolski <pawel.nadolski@gmail.com>
2011-05-09 20:38:20 +0200

    first solusion for WIE problem

--- Makefile
+++ Makefile
@@ -1,2 +1,14 @@
+PROFOPTS=-fprofile-arcs -ftest-coverage
+CCOPTS=-g -lm -static
+TARGETS=tul wie
+CC=gcc
+
 tul: tul.c
-	gcc -o tul tul.c
+	gcc $(PROFOPTS) $(CCOPTS) tul.c -o tul
+wie: wie.c
+	gcc $(PROFOPTS) $(CCOPTS) wie.c -o wie
+wie.test: wie
+	./wie < wie.in > wie.out
+	gcov wie.c
+clean:
+	rm -f *.gcda *.gcno *.gcov *.out $(TARGETS)
---
+++ wie.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdio.h>
+#define MAX 1000
+char board[MAX][MAX+1];
+char x[MAX+1], y[MAX+1];
+int main(int argc, char *argv[])
+{
+  int n, i, j;
+  scanf("%d\n", &n);
+  memset(x, '.', n);
+  memset(y, '.', n);
+  for(i=0; i<n; i++)
+  {
+    scanf("%s\n", board[i]);
+    int found_W = 0;
+    for(j=0; j<n; j++)
+    {
+      if(board[i][j] == 'W')
+      {
+        x[j] = 'W';
+        found_W = 1;
+      }
+    }
+    if(found_W == 1)
+      y[i] = 'W';
+  }
+  int curx = 0;
+  for(i=0; i<n; i++)
+  {
+    if(y[i] != 'W')
+    {
+      /*printf("enter i: %d\n", i);*/
+      for(j=curx; j<n; j++, curx++)
+      {
+        if(x[curx] != 'W')
+          break;
+      }
+      board[i][curx++] = 'W';
+      /*printf("%d %d\n", i, curx);*/
+    }
+    board[i][n] = '\0';
+    printf("%s\n", board[i]);
+  }
+  return 0;
+}
