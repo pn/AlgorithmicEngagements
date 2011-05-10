e60863c40143f6c123fad57a609d88251caf2afc
Paweł Nadolski <pawel.nadolski@gmail.com>
2011-05-10 22:05:00 +0200

    wsp prototype passes 100% tests

--- wsp.py
+++ wsp.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python
-import sys
-L = 10**9
 n = int(raw_input())
 a = []
 for i in xrange(n):
 	a.append(map(int, raw_input().split()))
-left = a[0][1]
-right = a[0][0]
 result = 0
+failed_join = True
 for i in xrange(1, len(a)):
+	if failed_join:
+		left = a[i-1][1]
+		right = a[i-1][0]
+		failed_join = False
+
 	left_level = a[i-1][1]
 	s = a[i][0] + a[i][1]
 	if left_level < s:
@@ -17,7 +19,6 @@ for i in xrange(1, len(a)):
 		left, right = right, left
 		left = min(left, a[i][1])
 		result+=1
-		sys.stderr.write('right %d\n' % result)
 	else:
 		diff = left_level - s
 		if left >= diff:
@@ -28,6 +29,7 @@ for i in xrange(1, len(a)):
 			left, right = right, left
 			left = 0
 			result+=1
-			sys.stderr.write('left %d\n' % result)
+		else:
+			failed_join = True
 
 print(result)
