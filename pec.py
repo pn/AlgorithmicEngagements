9e37073df6bbb0f84da7062878dc3cdfddb7d4d5
Paweł Nadolski <pawel.nadolski@gmail.com>
2011-05-11 19:26:22 +0200

    pec passes 3% tests

--- pec.py
+++ pec.py
@@ -1,20 +1,25 @@
 #!/usr/bin/env python
 import sys
 (s, w, k) = map(float, raw_input().split())
+sys.stderr.write("%f %f %f\n" % (s, w, k))
 n = w//k
-#if w == 27:
-#	n+=1
 result = 0
 while n > 0 and s > 0:
 	if n == 1:
 		result = w - s
 		break
 	x = k / (2*n - 1)
+	sys.stderr.write('w: %f x: %f s: %f n: %f\n' % (w, x, s, n))
 	if x > s:
 		x = s
+	k2 = k
+	if w % k >= 2*x:
+		sys.stderr.write("%f\n" % (w % k))
+		n+=1
+		k2 = w/n
+	w = n*k2 - x*(2*n-1)
+	sys.stderr.write('w: %f x: %f s: %f n: %f\n' % (w, x, s, n))
 	s -= x
-	#w = n*(w//n) - x*(2*n-1)
-	w = n*k - x*(2*n-1)
 	if not w:
 		break
 	result = w
