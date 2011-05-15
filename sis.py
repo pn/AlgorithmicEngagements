1f61666378fec9cc9a2b1a253aaaa674d0f3b1d9
Paweł Nadolski <pawel.nadolski@gmail.com>
2011-05-15 17:02:28 +0200

    update sis.py

--- sis.py
+++ sis.py
@@ -1,8 +1,5 @@
 #!/usr/bin/env python
-import sys
 k = int(raw_input())
-#for i in xrange(k):
-#	print([int(j) for j in range(1, i+2)])
 s = 0
 p = 5
 while p <= k:
@@ -10,5 +7,4 @@ while p <= k:
 	r = k % p
 	s += (r+1)*d + p*(d*(d-1))/2
 	p *= 5
-sys.stdout.write(str(s))
-#print s
+print s
