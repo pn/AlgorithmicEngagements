99b567b41041571bbb81d15aa02e0a24989ff073
Paweł Nadolski <pawel.nadolski@gmail.com>
2011-05-17 18:56:51 +0200

    update pec.py

--- pec.py
+++ pec.py
@@ -4,26 +4,29 @@ import sys
 sys.stderr.write("%f %f %f\n" % (s, w, k))
 n = w//k
 result = 0
-while n > 0 and s > 0:
-	if n == 1:
+while s > 0:
+	#sys.stderr.write('n: %f s: %f\n' % (n, s))
+	if n <= 1:
 		result = w - s
 		break
 	x = k / (2*n - 1)
-	sys.stderr.write('w: %f x: %f s: %f n: %f\n' % (w, x, s, n))
+	#sys.stderr.write('w: %f x: %f s: %f n: %f\n' % (w, x, s, n))
 	if x > s:
 		x = s
 	k2 = k
 	if w % k >= 2*x:
-		sys.stderr.write("%f\n" % (w % k))
+		#sys.stderr.write("%f\n" % (w % k))
 		n+=1
 		k2 = w/n
 	w = n*k2 - x*(2*n-1)
-	sys.stderr.write('w: %f x: %f s: %f n: %f\n' % (w, x, s, n))
+	#sys.stderr.write('w: %f x: %f s: %f n: %f\n' % (w, x, s, n))
 	s -= x
 	if not w:
 		break
 	result = w
-	n = w//k
-	sys.stderr.write('w: %f x: %f s: %f n: %f\n' % (w, x, s, n))
-
-print("%.3f" % result)
+	#sys.stderr.write('w: %f x: %f s: %f n: %f\n' % (w, x, s, n))
+	n -= 1
+if result < 0:
+	print("NIE")
+else:
+	print("%.3f\n" % result)
